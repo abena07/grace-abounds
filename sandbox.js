253397117e178700f33523dc1432d5c3fc535bc4
abena07 <62352130+abena07@users.noreply.github.com>
2021-07-10 16:05:50 +0000

    lip

--- sandbox.js
+++ sandbox.js
@@ -10,3 +10,17 @@
 let count = 0 //when storing data into a variable you type out let count be 0
 console.log(count)
 // after a document is linked to it's html by referencing the script tag to it's source you can get the html  by document.getElementById("count").innerHtml= 0
+
+
+///initializing exercise
+let bonusPoint = 50
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint + 50
+console.log(bonusPoint)
+
+bonusPoint = bonusPoint - 75
+console.log(bonusPoint)
+
+bonusPoint =bonusPoint + 45
+console.log(bonusPoint)
