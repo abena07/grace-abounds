d26023e80aabdb418859efea27315e4d61138c34
abena07 <62352130+abena07@users.noreply.github.com>
2021-07-10 15:45:37 +0000

    lip

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+// var character = 'mario';
+// console.log(character);
+// var inputs = document.querySelectorAll('input');
+// inputs.forEach(function (input) {
+//     console.log(input);
+// });
+
+
+///7/10/2021
+let count = 0 //when storing data into a variable you type out let count be 0
+console.log(count)
+// after a document is linked to it's html by referencing the script tag to it's source you can get the html  by document.getElementById("count").innerHtml= 0
