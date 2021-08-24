632f6913d69e6068098d3044deac42f7364afdb1
abena07 <62352130+abena07@users.noreply.github.com>
2021-08-24 03:13:00 +0000

    wip

---
+++ public/sandbox.js
@@ -0,0 +1,34 @@
+"use strict";
+var circumference = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circumference(2.9));
+//arrays
+var people = ['luigi', 'mario', 'yoshi'];
+people.push('toad');
+// you can only add a different type when you declare an array with a different types
+var mixed = ['cat', 7, 19, 'dog', 9, 'mouse'];
+mixed.push('goat');
+mixed[1] = "bark";
+//union types
+var mix = [];
+mixed.push('hello');
+mixed.push(20);
+var uid;
+uid = '123';
+uid = 223;
+//objects but with objects you can also specify what type they shuld be 
+var babe = {
+    name: 'mario',
+    belt: 'black',
+    age: 40
+};
+/// there is also an any type..
+var age = 25;
+age = true;
+console.log(age);
+//so when you set an object to any you can push any data type in that array and the same for objects
+//with declaring a value you use the colon cos you are assignin it
+var ninja;
+ninja = { name: 24, age: "yoshi" };
+console.log(ninja);
---
+++ src/sandbox.ts
@@ -0,0 +1,42 @@
+const circumference = (diameter: number) => {
+    return diameter * Math.PI;
+}
+
+console.log(circumference(2.9));
+
+//arrays
+let people = ['luigi', 'mario', 'yoshi']
+people.push('toad')
+// you can only add a different type when you declare an array with a different types
+let mixed = ['cat', 7, 19, 'dog', 9, 'mouse']
+mixed.push('goat')
+mixed[1] = "bark"
+
+//union types
+let mix: (string | number | boolean)[] = []
+mixed.push('hello')
+mixed.push(20)
+
+
+let uid: string | number;
+uid = '123';
+uid = 223;
+//objects but with objects you can also specify what type they shuld be 
+
+let babe = {
+    name: 'mario',
+    belt: 'black',
+    age: 40
+}
+
+/// there is also an any type..
+let age: any = 25;
+age = true
+console.log(age)
+//so when you set an object to any you can push any data type in that array and the same for objects
+//with declaring a value you use the colon cos you are assignin it
+let ninja: {
+    name: any, age: any
+}
+ninja = { name: 24, age: "yoshi" }
+console.log(ninja)
