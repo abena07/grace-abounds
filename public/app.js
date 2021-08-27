1c56bc41bb97462466f81f2030809d828c1e7a7a
abena07 <62352130+abena07@users.noreply.github.com>
2021-08-27 05:18:11 +0000

    wip

---
+++ public/app.js
@@ -0,0 +1,3 @@
+"use strict";
+// const anchor = document.querySelector('a')!;
+var form = document.querySelector('.new-item-form');
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+// const anchor = document.querySelector('a')!;
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
