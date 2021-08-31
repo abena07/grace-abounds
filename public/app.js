7032e51aee20b197f19fea48f5c03f30d9fb96b5
abena07 <62352130+abena07@users.noreply.github.com>
2021-08-31 03:24:45 +0000

    lez get it

--- public/app.js
+++ public/app.js
@@ -1,3 +1,12 @@
 "use strict";
 // const anchor = document.querySelector('a')!;
 var form = document.querySelector('.new-item-form');
+//inputs
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
