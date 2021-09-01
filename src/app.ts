88cb2e452b97bdc98b411088dc57436cd7203c87
abena07 <62352130+abena07@users.noreply.github.com>
2021-09-01 07:03:05 +0000

    lezgo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 // const anchor = document.querySelector('a')!;
+//classes 
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
 
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format() {
+        return `$(this.client) owes $ $(this.amount) for $(this.details)`;
+    }
+}
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
