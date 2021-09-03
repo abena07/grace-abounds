210a452f88b61fa250dcf97b6cd87ed6722af457
abena07 <62352130+abena07@users.noreply.github.com>
2021-09-03 21:30:07 +0000

    let's keep going

--- public/app.js
+++ public/app.js
@@ -1,7 +1,24 @@
 "use strict";
 // const anchor = document.querySelector('a')!;
+//classes 
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "$(this.client) owes $ $(this.amount) for $(this.details)";
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
 var form = document.querySelector('.new-item-form');
-//inputs
+//inputs //using the as is called type casting
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
 var details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,13 @@ class Invoice {
     }
 }
 
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 
